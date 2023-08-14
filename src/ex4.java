f180deb5704348665ab0ee381c4b8f417d50f6ee
PET0151153 <joao.m.silva13@aluno.senai.br>
2023-08-14 08:42:45 -0300

    exercicio 4

--- src/ex4.java
+++ src/ex4.java
@@ -4,15 +4,15 @@ public class ex4 {
 //    ex4
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        System.out.println("Qual o número do funcionário: ");
+        System.out.print("Qual o número do funcionário: ");
         int funcionario_num = input.nextInt();
         input.nextLine();
 
-        System.out.println("Horas trabalhadas: ");
+        System.out.print("Horas trabalhadas: ");
         float horas_trabalhadas = input.nextFloat();
         input.nextLine();
 
-        System.out.println("quanto voce recebe por hora: ");
+        System.out.print("quanto voce recebe por hora: ");
         float recebe_hora = input.nextFloat();
         input.nextLine();
 
